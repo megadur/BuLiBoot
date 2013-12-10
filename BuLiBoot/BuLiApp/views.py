ed54ff0f1dccf601b16cd1af50c1be8202ce55ba
megadur <megadur@gmail.com>
2013-12-10 08:50:26 +0100

    init

---
+++ BuLiBoot/BuLiApp/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import FormView
+from django.views.generic.base import TemplateView
+from django.contrib import messages
+
+#from .forms import ContactForm
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        messages.info(self.request, 'This is a demo of a message.')
+        return context
+
+
