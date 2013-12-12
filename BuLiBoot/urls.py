f592eb45f4faea9fe4f72f22b3ef17b0370fb47e
megadur <megadur@gmail.com>
2013-12-12 22:01:54 +0100

    tsss

---
+++ BuLiBoot/urls.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, include, url
+
+#from .views import HomePageView, FormView, FormHorizontalView, FormInlineView, PaginationView
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'BuLiBoot.views.home', name='home'),
+    #url(r'^$', include('BuLiApp.urls')),
+    url(r'^BuLiBoot/', include('BuLiApp.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
